242374cdee801a686185580fe9b7dca8383a822b
demon5232 <azuresimca@gmail.com>
2017-09-07 13:30:17 +0800

    github commit test

--- app/src/main/java/com/mendokuse/afternoonteaorder/StoreActivity.java
+++ app/src/main/java/com/mendokuse/afternoonteaorder/StoreActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,7 +18,7 @@ public class StoreActivity extends AppCompatActivity {
         setContentView(R.layout.activity_store);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        Log.d("test","githubtest");
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
